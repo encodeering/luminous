a794bf6db3253d42d91b8c864728549c091d73b9
Michael Clausen <encodeering@gmail.com>
2021-03-29 15:22:55 +0200

    implements a serialization container for a instrument message
    * uses a sample from the webbrowser stream

--- module/application/src/main/kotlin/com/encodeering/luminous/application/internal/partner/instrument/InstrumentRoute.kt
+++ module/application/src/main/kotlin/com/encodeering/luminous/application/internal/partner/instrument/InstrumentRoute.kt
@@ -1,6 +1,7 @@
 package com.encodeering.luminous.application.internal.partner.instrument
 
 import com.encodeering.luminous.application.api.partner.PartnerConfig
+import kotlinx.serialization.Serializable
 import org.apache.camel.builder.RouteBuilder
 import javax.enterprise.context.ApplicationScoped
 
@@ -13,5 +14,16 @@ internal class InstrumentRoute (private val config: PartnerConfig): RouteBuilder
     override fun configure () {
         from ("ahc-ws://${config.endpoint}/instruments").routeId ("partner-instruments").to ("log:instruments")
     }
+
+}
+
+@Serializable
+internal data class InstrumentMessage (val data: Data, val type: Type) {
+
+    @Serializable
+    data class Data (val isin: String, val description: String)
+
+    @Serializable
+    enum class Type { ADD, DELETE }
 
 }
---
+++ module/application/src/test/kotlin/com/encodeering/luminous/application/internal/partner/instrument/InstrumentMessageTest.kt
@@ -0,0 +1,42 @@
+package com.encodeering.luminous.application.internal.partner.instrument
+
+import kotlinx.serialization.SerializationException
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EmptySource
+import org.junit.jupiter.params.provider.NullSource
+import org.junit.jupiter.params.provider.ValueSource
+
+/**
+ * @author clausen - encodeering@gmail.com
+ */
+internal class InstrumentMessageTest {
+
+    @ParameterizedTest
+    @ValueSource (strings = ["ADD", "DELETE"])
+    fun `message should be readable` (type: String) {
+        assertThat (Json.decodeFromString<InstrumentMessage> (json (type))).isEqualTo (InstrumentMessage (InstrumentMessage.Data ("VE1506683Q53", "convallis quis convenire malorum arcu dolore"), InstrumentMessage.Type.valueOf (type)))
+    }
+
+    @ParameterizedTest
+    @EmptySource
+    @NullSource
+    @ValueSource (strings = ["OTHER"])
+    fun `message should be no readable for other types` (type: String?) {
+        assertThrows<SerializationException> { Json.decodeFromString<InstrumentMessage> (json (type)) }
+    }
+
+    private fun json (type: String?) = """
+        {
+          "data": {
+            "description": "convallis quis convenire malorum arcu dolore",
+            "isin": "VE1506683Q53"
+          },
+          "type": "$type"
+        }
+    """.trimIndent ()
+
+}
