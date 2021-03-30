8833880f729dc193603b0c82f33c556168da491d
Michael Clausen <encodeering@gmail.com>
2021-03-30 13:56:25 +0200

    provides price information as part of instrument rest api
    * last known price with timestamp will be exposed or null if not yet present

--- module/application/src/main/kotlin/com/encodeering/luminous/application/internal/partner/instrument/InstrumentWeb.kt
+++ module/application/src/main/kotlin/com/encodeering/luminous/application/internal/partner/instrument/InstrumentWeb.kt
@@ -2,11 +2,15 @@ package com.encodeering.luminous.application.internal.partner.instrument
 
 import com.encodeering.luminous.application.api.partner.instrument.Instrument
 import com.encodeering.luminous.application.api.partner.instrument.InstrumentRepository
+import com.encodeering.luminous.application.api.partner.quote.Quote
+import com.encodeering.luminous.application.api.partner.quote.QuoteRepository
 import io.quarkus.vertx.web.Param
 import io.quarkus.vertx.web.Route
 import io.quarkus.vertx.web.RouteBase
 import io.quarkus.vertx.web.RoutingExchange
 import io.vertx.core.json.Json
+import java.math.BigDecimal
+import java.time.OffsetDateTime
 import javax.enterprise.context.ApplicationScoped
 
 /**
@@ -14,16 +18,30 @@ import javax.enterprise.context.ApplicationScoped
  */
 @ApplicationScoped
 @RouteBase (path = "/instrument", produces = ["application/json"])
-internal class InstrumentWeb (private val instruments: InstrumentRepository) {
+internal class InstrumentWeb (private val instruments: InstrumentRepository, private val quotes: QuoteRepository) {
 
     @Route (path = "/")
-    fun instruments (): Set<Instrument> =
-        instruments.all ()
+    fun instruments (): List<InstrumentEntity> =
+        instruments.all ().map { InstrumentEntity (it, quotes.last (it.isin)) }
 
     @Route (path = "/:isin")
     fun instrument (@Param ("isin") isin: String, exchange: RoutingExchange): Unit = when (val instrument = instruments.one (isin)) {
         null -> exchange.notFound ().end ()
-        else -> exchange.ok (Json.encode (instrument))
+        else -> exchange.ok (Json.encode (InstrumentEntity (instrument, quotes.last (instrument.isin))))
     }
+
+}
+
+internal data class InstrumentEntity (val isin: String, val description: String, val price: Map<String, Price>?) {
+
+    constructor (instrument: Instrument, quote: Quote?): this (
+        instrument.isin,
+        instrument.description,
+        quote?.run {
+            mapOf ("last" to Price (price, timestamp))
+        }
+    )
+
+    data class Price (val price: BigDecimal, val timestamp: OffsetDateTime)
 
 }
--- module/application/src/test/kotlin/com/encodeering/luminous/application/internal/partner/instrument/InstrumentWebTest.kt
+++ module/application/src/test/kotlin/com/encodeering/luminous/application/internal/partner/instrument/InstrumentWebTest.kt
@@ -2,6 +2,9 @@ package com.encodeering.luminous.application.internal.partner.instrument
 
 import com.encodeering.luminous.application.api.partner.instrument.Instrument
 import com.encodeering.luminous.application.api.partner.instrument.InstrumentRepository
+import com.encodeering.luminous.application.api.partner.quote.Quote
+import com.encodeering.luminous.application.api.partner.quote.QuoteRepository
+import com.encodeering.luminous.application.internal.partner.quote.QuoteRepositoryMemory
 import io.quarkus.test.Mock
 import io.quarkus.test.common.http.TestHTTPEndpoint
 import io.quarkus.test.junit.QuarkusTest
@@ -9,6 +12,7 @@ import io.restassured.RestAssured.given
 import io.vertx.core.json.Json
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import java.time.OffsetDateTime
 import javax.enterprise.context.ApplicationScoped
 import javax.enterprise.inject.Produces
 
@@ -31,11 +35,18 @@ internal class InstrumentWebTest {
         [
           {
             "isin": "VE1506683Q53",
-            "description": "hello"
+            "description": "hello",
+            "price": {
+              "last": {
+                "price": 345.456,
+                "timestamp": "2021-03-30T13:47:19+02:00"
+              }
+            }
           },
           {
             "isin": "LF681P504335",
-            "description": "world"
+            "description": "world",
+            "price": null
           }
         ]
         """.trimIndent ()))
@@ -52,7 +63,13 @@ internal class InstrumentWebTest {
         assertThat (Json.decodeValue (body)).isEqualTo (Json.decodeValue ("""
         {
           "isin": "VE1506683Q53",
-          "description": "hello"
+          "description": "hello",
+          "price": {
+            "last": {
+              "price": 345.456,
+              "timestamp": "2021-03-30T13:47:19+02:00"
+            }
+          }
         }
         """.trimIndent ()))
     }
@@ -73,6 +90,17 @@ internal class InstrumentWebTest {
         fun instruments (): InstrumentRepository = InstrumentRepositoryMemory ().apply {
             add (Instrument ("VE1506683Q53", "hello"))
             add (Instrument ("LF681P504335", "world"))
+        }
+
+        @Produces
+        @Mock
+        fun quotes (): QuoteRepository = QuoteRepositoryMemory ().apply {
+            val timestamp = OffsetDateTime.parse ("2021-03-30T13:46:19.00+02:00")
+
+            remember   ("VE1506683Q53")
+
+            add (Quote ("VE1506683Q53", "234.345".toBigDecimal (), timestamp))
+            add (Quote ("VE1506683Q53", "345.456".toBigDecimal (), timestamp.plusMinutes (1)))
         }
 
     }
