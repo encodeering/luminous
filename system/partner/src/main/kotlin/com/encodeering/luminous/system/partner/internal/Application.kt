07c2ee6bfff7d8aabe6f4bc6749286926d91daab
Michael Clausen <encodeering@gmail.com>
2022-11-29 11:53:47 +0100

    binds the marketplace module
    * runs the module when the application starts

--- system/partner/src/main/kotlin/com/encodeering/luminous/system/partner/internal/Application.kt
+++ system/partner/src/main/kotlin/com/encodeering/luminous/system/partner/internal/Application.kt
@@ -1,9 +1,12 @@
 package com.encodeering.luminous.system.partner.internal
 
 import com.encodeering.luminous.system.partner.internal.ktor.Module
+import com.encodeering.luminous.system.partner.internal.marketplace.MarketplaceModule
 import com.encodeering.luminous.system.partner.internal.metric.MetricModule
 import io.ktor.server.application.Application
 import io.ktor.server.netty.EngineMain
+import java.time.Clock
+import java.util.concurrent.TimeUnit.MILLISECONDS
 
 /**
  * @author clausen - encodeering@gmail.com
@@ -14,11 +17,14 @@ internal fun Application.module () = ApplicationModule (this).load ()
 
 internal class ApplicationModule (application: Application): Module {
 
+    val clock: Clock = Clock.systemUTC ()
 
     val metric = MetricModule (application)
+    val marketplace = MarketplaceModule (clock, application, rate = 100, unit = MILLISECONDS)
 
     private fun all () = listOf (
-        metric
+        metric,
+        marketplace
     )
 
     override fun load () {
--- system/partner/src/test/kotlin/com/encodeering/luminous/system/partner/internal/ApplicationTest.kt
+++ system/partner/src/test/kotlin/com/encodeering/luminous/system/partner/internal/ApplicationTest.kt
@@ -1,13 +1,31 @@
 package com.encodeering.luminous.system.partner.internal
 
+import com.encodeering.luminous.system.partner.internal.ktor.events
+import com.encodeering.luminous.system.partner.internal.ktor.started
+import com.encodeering.luminous.system.partner.internal.ktor.stopped
 import com.encodeering.luminous.system.partner.testing.ktor.Demos.demo
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import java.util.concurrent.atomic.AtomicBoolean
 
 internal class ApplicationTest {
 
     @Test
     fun `should start and stop properly` () {
-        demo (file = "application.conf") {}
+        val started = AtomicBoolean (false)
+        val stopped = AtomicBoolean (false)
+
+        demo (file = "application.conf") {
+            application {
+                events {
+                    started { started.set (true) }
+                    stopped { stopped.set (true) }
+                }
+            }
+        }
+
+        assertThat (started.get ()).isTrue ()
+        assertThat (stopped.get ()).isTrue ()
     }
 
 }
